4a59771bd6ca0e0c43fbcd526f734d07c8004c08
StevanEarl <stevan.earl@asu.edu>
2018-03-08 18:04:21 -0700

    developed a working version that interacts with sync

--- data-processing/data_homogonization.R
+++ data-processing/data_homogonization.R
@@ -3,6 +3,9 @@
 library(googledrive)
 library(googlesheets)
 library(tidyverse)
+library(readxl)
+library(readODS)
+library(tools)
 
 
 # todos and notes ----
@@ -14,6 +17,7 @@ library(tidyverse)
 # warnings to file
 # on-run warns for: 
   # multiple header_names of same name
+# need to clear temporaryWorkspace between runs
 
 
 # generic function ----
@@ -43,46 +47,73 @@ library(tidyverse)
 
 # helper functions
 
-# batch googlesheets file ingestion function
+# batch load googlesheets file ingestion function
+# batch_load <- function(fileName, skipRows, missingValueCode) {
+#   token <- gs_title(fileName)
+#   dataFile <- gs_read(token, skip = skipRows, na = missingValueCode)
+# }
+
+# batch load synced googlesheets file ingestion function
 batch_load <- function(fileName, skipRows, missingValueCode) {
-  token <- gs_title(fileName)
-  dataFile <- gs_read(token, skip = skipRows, na = missingValueCode)
+  if (file_ext(fileName) == 'ods') {
+    dataFile <- read_ods(fileName, skip = skipRows, na = missingValueCode)
+  } else if (file_ext(fileName) == 'csv') {
+    dataFile <- read_csv(fileName, skip = skipRows, na = missingValueCode)
+  } else if (grepl('xls', file_ext(fileName))) {
+    dataFile <- read_excel(fileName, skip = skipRows, na = missingValueCode)
+  } else { print(" --- data file type compatability error ---")}
 }
 
-
 data_homogonization <- function(directoryName) {
+  
+  # set the file reference to the identified directory
+  setwd(directoryName)
   
   # Google Drive directory
   
   # access Google directory id for reference
-  googleID <- drive_get(directoryName) %>% 
+  # not avaialable locally, must be pulled from Google API
+  googleID <- drive_get(basename(directoryName)) %>% 
     pull(id)
   
   # list files in Google directory 
-  dirFileList <- drive_ls(path = directoryName) 
+  # dirFileList <- list.files(directoryName) 
+  dirFileNames <- list.files(directoryName) 
   
   # isolate names from Google directory 
-  dirFileNames <- dirFileList %>% 
-    select(name) %>% 
-    pull(name)
+  # dirFileNames <- dirFileList %>% 
+  #   select(name) %>% 
+  #   pull(name)
   
   
   # Key File  
   
   # isolate key-key and extract details in location and profile tabs
   keyFileName <- grep("key", dirFileNames, ignore.case = T, value = T)
-  keyFileToken <- gs_title(keyFileName)
+  # keyFileToken <- gs_title(keyFileName)
   
-  locationData <- gs_read(keyFileToken, ws = 1) %>% 
+  
+  locationData <- read_ods(keyFileName, sheet = 1) %>% 
     filter(!is.na(Value)) %>% 
     add_row(Value = googleID, var = 'google_id', .before = 1)
   
-  profileData <- gs_read(keyFileToken, ws = 2) %>% 
+  # Google API approach
+  # locationData <- gs_read(keyFileToken, ws = 1) %>% 
+  #   filter(!is.na(Value)) %>% 
+  #   add_row(Value = googleID, var = 'google_id', .before = 1)
+  
+  profileData <- read_ods(keyFileName, sheet = 2) %>% 
     filter(!is.na(header_name))
   
-  # isolate rows to skip from locationData for data import
+  # Google API approach
+  # profileData <- gs_read(keyFileToken, ws = 2) %>% 
+  #   filter(!is.na(header_name))
+  
+  # isolate rows to skip from locationData for data import. this was originally
+  # intended to be an input as to the number of rows to skip but it seems to
+  # have been interpreted as the row number of the header.
   if(length(locationData[locationData$var == 'header_row',]$var) == 1) { 
-    skipRows = locationData[locationData$var == 'header_row',]$Value
+    skipRows = as.numeric(locationData[locationData$var == 'header_row',]$Value) - 1
   } else {
     skipRows = 0
   }
@@ -107,11 +138,16 @@ data_homogonization <- function(directoryName) {
                           missingValueCode = missingValueCode,
                           skipRows = skipRows)
   
+  
   # add filenames
   names(googleDirData) <- dirFileNames
   
   # as key file is already loaded, remove it from the list of data frames 
-  googleDirData <- googleDirData[-grepl("key", names(googleDirData), ignore.case = T)]
+  googleDirData <- googleDirData[!grepl("key", names(googleDirData), ignore.case = T)]
+  
+  # print header rows as a quality-control step
+  lapply(googleDirData, function(frame) {
+    print(head(frame, n = 1L)) })
 
   # generate a vector of dataframe columns to keep from key file input to
   # header_name
@@ -123,7 +159,8 @@ data_homogonization <- function(directoryName) {
   googleDirData <- map(googleDirData, select, one_of(varsToKeep))
   
   # rename investigator names to key file names
-  googleDirData <- lapply(googleDirData, function(frame) { setNames(frame, profileData$var[match(names(frame), profileData$header_name)]) })
+  googleDirData <- lapply(googleDirData, function(frame) { 
+    setNames(frame, profileData$var[match(names(frame), profileData$header_name)]) })
   
   # generate wide data frame of location data
   locationDataWide <- locationData %>% 
@@ -131,34 +168,67 @@ data_homogonization <- function(directoryName) {
     spread(key = var, value = Value)
 
   # merge location data with each data frame
-  googleDirData <- lapply(googleDirData, function(frame) { merge(locationDataWide, frame, all = T) })
+  googleDirData <- lapply(googleDirData, function(frame) { 
+    merge(locationDataWide, frame, all = T) })
   
   # rename files to include base name + indication of homogenization 
   names(googleDirData) <- paste0(str_extract(names(googleDirData), "^[^\\.]*"), "_HMGZD")
   
   # Data and file output 
   
+  # create a temporary, local workspace if it does not already exist
+  
+  temporaryDirectory <- '~/Desktop/temp_som_outspace/'
+  
+  if(!dir.exists(temporaryDirectory)) {
+    dir.create(temporaryDirectory)
+  }
+  
   # write files to a temporary location
   googleDirData %>%
     names(.) %>%
-    # map(~ write_csv(googleDirData[[.]], paste0("~/Desktop/temp_som_outspace/", .)))
-    map(~ write_csv(googleDirData[[.]], paste0("~/Desktop/temp_som_outspace/", ., ".csv")))
+    map(~ write_csv(googleDirData[[.]], paste0(temporaryDirectory, ., ".csv")))
+  
+  # write files to the synced google directory - this works but even with the
+  # csv extension, they are viewed as text files and, though open- and edit-able
+  # with Google Sheets, you run into that problem of Google creating a new
+  # googlesheet file
+  # googleDirData %>%
+  #   names(.) %>%
+  #   map(~ write_csv(googleDirData[[.]], paste0(directoryName, "/", ., ".csv")))
+  
+  # write the files from the temporary directory to the appropriate Google
+  # Directory
+  filesToUpload <- list.files(path=temporaryDirectory,
+                              full.names=F,
+                              recursive=FALSE)
+  
+  lapply(filesToUpload, function(frame) {
+    drive_upload(paste0(temporaryDirectory, frame),
+                 path = drive_get(as_id(googleID)), type = "spreadsheet")
+  })
   
   
-  # temp_som_hmgzd_output
-
 }
 
 
 tempToGoogleDrive <- function() {
   
   # identify directory with files (not full.names=T)
-  filesToUpload <- list.files(path="~/Desktop/temp_som_outspace/",
+  filesToUpload <- list.files(path=temporaryDirectory,
                               full.names=F,
                               recursive=FALSE)
 
-  filesToUpload %>% 
-    names(.)
-  drive_upload('event1513rsvps.csv', path = "temp_som_hmgzd_output/", name = "", type = "spreadsheet")
+  googleDirData %>%
+    names(.) %>%
+    map(~ write_csv(googleDirData[[.]], paste0(temporaryDirectory, ., ".csv")))
   
+  filesToUpload %>% 
+    names(.) %>%
+    map(~ drive_upload(filesToUpload[[.]], path = drive_get(as_id(googleID)), type = "spreadsheet"))
+ 
+  lapply(filesToUpload, function(frame) {
+    drive_upload(paste0(temporaryDirectory, frame), path = drive_get(as_id(googleID)), type = "spreadsheet")
+  })
+   
 }
