929580098b7ec3a0650ae3192ab3580544bc643a
StevanEarl <stevan.earl@asu.edu>
2018-05-21 21:11:53 -0700

    aligned google_sync branch code to mirror that in soilHomogenization package

--- data-processing/data_homogonization.R
+++ data-processing/data_homogonization.R
@@ -9,48 +9,6 @@ if(!grepl("Darwin", Sys.info()["sysname"])) {
 library(tools)
 
 
-# todos and notes ----
-
-# check header_row, 1 if NA - done!
-# for header_name to var consider rename_at, rename_all, or purrr::set_names - done!
-# check out select(matches) if select_one poses a problem - done!
-# location data are being sorted alphabetically, not a huge problem but need to correct
-# warnings to file
-# on-run warns for: 
-  # multiple header_names of same name
-# need to clear temporaryWorkspace between runs - done!
-# Run Sarah's CRD E133 first
-
-#' @title data_homogonization
-#'
-#' @description data_homogonization standardizes data files in a Google Drive
-#'   directory according to a user-supplied template loosely based on the Powell
-#'   Center Template
-#'
-#' @param directoryName Path to a Google Drive directory where data and a key
-#'   file (the template are housed)
-#'
-#' @import tidyverse
-#' @import googlesheets
-#' @import googledrive
-#'
-#' @return Returns a homogonized data file for each input data file
-#'
-#' @examples
-#' \dontrun{
-#' data_homogonization('CAP_LTER')
-#' }
-#'
-#' @export
-
-# helper functions
-
-# batch load googlesheets file ingestion function
-# batch_load <- function(fileName, skipRows, missingValueCode) {
-#   token <- gs_title(fileName)
-#   dataFile <- gs_read(token, skip = skipRows, na = missingValueCode)
-# }
-
 # batch load synced googlesheets file ingestion function
 batch_load <- function(fileName, skipRows, missingValueCode) {
   if (file_ext(fileName) == 'ods') {
@@ -75,10 +33,6 @@ unitsConversionProfile <- gs_read(unitsConversionFile, ws = 2,
                                   range = 'A1:H546') %>% # ignore fractions for now 
   filter(!is.na(unit_levels)) %>% 
   select(unit_levels, Var_long, var, givenUnit, unitConversionFactor)
-
-
-
-
 
 
 data_homogonization <- function(directoryName, temporaryDirectory) {
@@ -115,7 +69,7 @@ data_homogonization <- function(directoryName, temporaryDirectory) {
   dirFileNames <- list.files(directoryName) 
   
   
-  # KEY FILE ----
+  # ACCESS KEY FILE
   
   # isolate key-key and extract details in location and profile tabs
   keyFileName <- grep("key", dirFileNames, ignore.case = T, value = T)
@@ -129,12 +83,12 @@ data_homogonization <- function(directoryName, temporaryDirectory) {
   profileData <- read_ods(keyFileName, sheet = 2) %>% 
     filter(!is.na(header_name))
   
-  # generate a note file from the key file
+  # GENERATE NOTE FILE (FROM THE KEY FILE)
   
   # create a note name with path to output directory, name of key file + _HMGZD_NOTES.csv
   notesFileName <- paste0(temporaryDirectory, file_path_sans_ext(keyFileName), "_HMGZD_NOTES.csv")
   
-  # capture notes from location and profile key-file tabs and write to file
+  # capture notes from location and profile key-file tabs
   notes <- bind_rows(
     locationData %>% 
       filter(!is.na(var_notes)) %>% 
@@ -147,24 +101,47 @@ data_homogonization <- function(directoryName, temporaryDirectory) {
       select(source, Var_long, var, var_notes)
   )
   
-  # OE approach - write file but can we write units conversions first?
-  # write_csv( 
-  #   bind_rows(
-  #     locationData %>% 
-  #       filter(!is.na(var_notes)) %>% 
-  #       mutate(source = "location") %>% 
-  #       select(source, Var_long, var, var_notes),
-  #     profileData %>% 
-  #       filter(!is.na(Notes) | !is.na(Comment)) %>% 
-  #       unite(col = var_notes, Notes, Comment, sep = ";") %>% 
-  #       mutate(source = "profile") %>% 
-  #       select(source, Var_long, var, var_notes)
-  #   ), notesFileName 
-  # )
+  # +++++++++++++++++++++++++++++++++++++++
+  # BEGIN STANDARDIZE UNITS::location data
+  # source('~/Dropbox/development/standardize_units_location.R')
   
-  # standardize units of location-level data  
-  # standardize_units_location()
-  source('~/Dropbox/development/standardize_units_location.R')
+  # location tab DATA containing units only
+  locationDataUnits <- locationData %>%
+    dplyr::filter(!is.na(Unit)) %>%
+    dplyr::select(Value, unit_levels = Unit, Var_long, var)
+  
+  # join location DATA with units and corresponding vars in conversion table
+  LDU_UCL <- dplyr::left_join(locationDataUnits, unitsConversionLocation,
+                              by = c("var", "unit_levels"),
+                              suffix = c(".PD", ".UT")) %>%
+    dplyr::filter(
+      !is.na(unitConversionFactor),
+      unitConversionFactor != 1
+    )
+  
+  # standardize location data units
+  for (varValue in c(LDU_UCL$var)) {
+    
+    # standardize values per the units_conversion_table
+    locationData[locationData$var == varValue,]['Value'] <- as.numeric(locationData[locationData$var == varValue,]['Value']) * LDU_UCL[LDU_UCL$var == varValue,]['unitConversionFactor']
+    
+    # add mention of conversions to notes
+    if (nrow(notes[notes$var == varValue,]) >= 1) {
+      notes <- notes %>%
+        dplyr::mutate(var_notes = replace(var_notes,
+                                          var == varValue,
+                                          paste0(var_notes, "; UNITS CONVERSION APPLIED: ", LDU_UCL[LDU_UCL$var == varValue,]['unitConversionFactor'])))
+    } else {
+      notes <- notes %>%
+        tibble::add_row(source = 'location',
+                        var = varValue,
+                        var_notes = paste0("UNITS CONVERSION APPLIED: ", LDU_UCL[LDU_UCL$var == varValue,]['unitConversionFactor']))
+    }
+  }
+  
+  # END STANDARDIZE UNITS::location data
+  # +++++++++++++++++++++++++++++++++++++++
+  
   
   # Isolate rows to skip from locationData for data import. This was originally
   # intended to be an input as to the number of rows to skip but it seems to
@@ -174,7 +151,7 @@ data_homogonization <- function(directoryName, temporaryDirectory) {
   } else {
     skipRows = 0
   }
- 
+  
   # isolate missing value codes from locationData for data import
   if (length(locationData[locationData$var == 'NA_1',]$var) == 1) {
     mvc1 = locationData[locationData$var == 'NA_1',]$Value }
@@ -187,9 +164,9 @@ data_homogonization <- function(directoryName, temporaryDirectory) {
   if (exists('mvc1') && exists('mvc2')) { missingValueCode = c(paste(mvc1, mvc2))}
   
   
-  # DATA FILE(S) ----
+  # DATA FILE(S)
   
-  # import all (data + key) files from the Google dir
+  # import all (data + key) files from the google dir
   googleDirData <- lapply(dirFileNames, 
                           batch_load, 
                           missingValueCode = missingValueCode,
@@ -205,30 +182,78 @@ data_homogonization <- function(directoryName, temporaryDirectory) {
   # print header rows to stdout as a quality-control step
   lapply(googleDirData, function(frame) {
     print(head(frame, n = 1L)) })
-
+  
   # generate a vector of dataframe columns to keep from key file input to
   # header_name
   varsToKeep <- profileData %>% 
     select(header_name) %>% 
     pull()
-
+  
   # pull targeted vars from each data frame based on header_names in key file
   googleDirData <- map(googleDirData, select, one_of(varsToKeep))
   
   # rename investigator-supplied names to key-file (standardized) names
   googleDirData <- lapply(googleDirData, function(frame) { 
     setNames(frame, profileData$var[match(names(frame), profileData$header_name)]) })
- 
-  # standardize units of profile-level data  
-  # standardize_units_profile()
-  source('~/Dropbox/development/standardize_units_profile.R')
   
- 
+  # +++++++++++++++++++++++++++++++++++++++
+  # BEGIN STANDARDIZE UNITS::profile data
+  # source('~/Dropbox/development/standardize_units_profile.R')
+  
+  # profile tab DATA containing units only
+  profileDataUnits <- profileData %>%
+    dplyr::filter(!is.na(unit_levels)) %>%
+    dplyr::select(header_name, unit_levels, Var_long, var)
+  
+  # join profile DATA with units and corresponding vars in conversion table
+  PDU_UCP <- dplyr::left_join(profileDataUnits, unitsConversionProfile,
+                              by = c("var", "unit_levels"),
+                              suffix = c(".PD", ".UT")) %>%
+    dplyr::filter(
+      !is.na(unitConversionFactor),
+      unitConversionFactor != 1
+    )
+  
+  # loop through all data frames in google dir
+  for (i in 1:length(googleDirData)) {
+    
+    for (dataCol in c(PDU_UCP$var)) {
+      
+      if (!is.null(googleDirData[[i]][[dataCol]])) {
+        
+        googleDirData[[i]][[dataCol]] <- googleDirData[[i]][[dataCol]] * PDU_UCP[PDU_UCP$var == dataCol,][['unitConversionFactor']]
+        
+        # add mention of conversions to notes
+        if (nrow(notes[notes$var == dataCol,]) >= 1) {
+          print(paste("mutate: ", dataCol))
+          
+          baseNote <- notes %>%
+            dplyr::filter(var == dataCol) %>%
+            dplyr::select(var_notes)
+          
+          notes <- notes %>%
+            dplyr::mutate(var_notes = replace(var_notes,
+                                              var == dataCol,
+                                              paste0(baseNote, "; UNITS CONVERSION APPLIED: ", PDU_UCP[PDU_UCP$var == dataCol,]['unitConversionFactor'])))
+        } else {
+          print(paste("add_row: ", dataCol))
+          notes <- notes %>%
+            tibble::add_row(source = 'profile',
+                            var = dataCol,
+                            var_notes = paste0("UNITS CONVERSION APPLIED: ", PDU_UCP[PDU_UCP$var == dataCol,]['unitConversionFactor']))
+        }
+      }
+    }
+  }
+  
+  # END STANDARDIZE UNITS::profile data
+  # +++++++++++++++++++++++++++++++++++++++
+  
   # generate wide data frame of location data
   locationDataWide <- locationData %>% 
     select(var, Value) %>% 
     spread(key = var, value = Value)
-
+  
   # merge location data with each data frame
   googleDirData <- lapply(googleDirData, function(frame) { 
     merge(locationDataWide, frame, all = T) })
@@ -237,7 +262,7 @@ data_homogonization <- function(directoryName, temporaryDirectory) {
   names(googleDirData) <- paste0(str_extract(names(googleDirData), "^[^\\.]*"), "_HMGZD")
   
   
-  # DATA FILE OUTPUT ----
+  # FILE OUTPUT
   
   # We can write the output directly to the working/target Google Directory. The
   # problem with that approach is that, even with a csv or otherwise file
@@ -260,16 +285,16 @@ data_homogonization <- function(directoryName, temporaryDirectory) {
   
   # write processed files to the temporary directory
   
-    # notes
-    write_csv(notes, notesFileName)
-    
-    # data
-    googleDirData %>%
-      names(.) %>%
-      map(~ write_csv(googleDirData[[.]], paste0(temporaryDirectory, ., ".csv")))
+  # notes
+  write_csv(notes, notesFileName)
+  
+  # data
+  googleDirData %>%
+    names(.) %>%
+    map(~ write_csv(googleDirData[[.]], paste0(temporaryDirectory, ., ".csv")))
   
   
-  # UPLOAD TO GOOGLE DRIVE ----
+  # UPLOAD OUTPUT TO GOOGLE DRIVE
   
   # list the files added to the temporary output space
   filesToUpload <- list.files(path=temporaryDirectory,
@@ -288,28 +313,7 @@ data_homogonization <- function(directoryName, temporaryDirectory) {
 
 
 # linux
-directoryName <- '/home/srearl/googleDrive/LTER-SOM/Data_downloads/UMBS_DIRT/SOIL_CN/UMBS_DIRT_C_N_by_Plot_2004_2014'
+directoryName <- '/home/srearl/googleDrive/LTER-SOM/Data_downloads/cap.557.9_151026011609313008/'
 
 # mac
 directoryName <- '~/Google Drive/LTER-SOM/Data_downloads/UMBS_DIRT/SOIL_CN/UMBS_DIRT_C_N_by_Plot_2004_2014/'
-
-# tempToGoogleDrive <- function() {
-#   
-#   # identify directory with files (not full.names=T)
-#   filesToUpload <- list.files(path=temporaryDirectory,
-#                               full.names=F,
-#                               recursive=FALSE)
-#   
-#   googleDirData %>%
-#     names(.) %>%
-#     map(~ write_csv(googleDirData[[.]], paste0(temporaryDirectory, ., ".csv")))
-#   
-#   filesToUpload %>% 
-#     names(.) %>%
-#     map(~ drive_upload(filesToUpload[[.]], path = drive_get(as_id(googleID)), type = "spreadsheet"))
-#   
-#   lapply(filesToUpload, function(frame) {
-#     drive_upload(paste0(temporaryDirectory, frame), path = drive_get(as_id(googleID)), type = "spreadsheet")
-#   })
-#   
-# }
